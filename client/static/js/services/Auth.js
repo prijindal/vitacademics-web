bdf63f321c5dd1c44da508f96bbb879b36ab93d5
prijindal <priyanshujindal1995@gmail.com>
2015-08-29 20:48:44 +0530

    production and development gulpfiles seperated

---
+++ client/static/js/services/Auth.js
@@ -0,0 +1,16 @@
+angular.module('VitApp')
+       .factory('Auth', function(){
+          var user;
+
+          return{
+              setUser : function(aUser){
+                  user = aUser;
+              },
+              isLoggedIn : function(){
+                  return(user)? user : false;
+              },
+              logout: function() {
+                user = undefined;
+              }
+            }
+        })
