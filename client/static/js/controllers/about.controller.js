89814aa5aede28810b5db8ffe2eef32b37ec19de
prijindal <priyanshujindal1995@gmail.com>
2015-09-02 22:27:24 +0530

    Small bugs fixed

--- client/static/js/controllers/about.controller.js
+++ client/static/js/controllers/about.controller.js
@@ -2,17 +2,5 @@ angular.module('VitApp')
        .controller('aboutController',['$scope', 'allAbout', function($scope, allAbout){
           var self = this;
           self.messages = allAbout.messages();
-          var ABOUT_INFO = allAbout.contributors();
-          self.contributors = ABOUT_INFO.slice(0, 2)
-          
-          var load_more = _.throttle((function() {
-            var new_to_load = Math.floor(window_layout.scrollTop() / 100);
-            
-            self.contributors = ABOUT_INFO.slice(0, 2 + new_to_load)
-            
-            $scope.$digest();
-          }), 0)
-
-          var window_layout = angular.element(document.getElementById('container'));
-          window_layout.on('scroll',load_more);
+          self.contributors = allAbout.contributors();
        }])
--- client/static/js/controllers/courses.controller.js
+++ client/static/js/controllers/courses.controller.js
@@ -2,17 +2,6 @@ angular.module('VitApp')
        .controller('coursesController',['$scope', 'allCourses', function($scope, allCourses){
            var self = this;         
 
-          var COURSES = allCourses.courses()
+          self.courses = allCourses.courses()
 
-          self.courses = COURSES.slice(0, 2);
-
-          var load_more = _.throttle((function() {
-            var new_to_load = Math.floor(window_layout.scrollTop() / 560);
-            self.courses = COURSES.slice(0, 2 + new_to_load)
-            
-            $scope.$digest();
-          }), 0)
-
-          var window_layout = angular.element(document.getElementById('container'));
-          window_layout.on('scroll',load_more);
         }])
